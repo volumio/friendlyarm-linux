07511dedfb4bea4261ba6e3b0252f65e92bfbcf6
nikkov <nikkov@gmail.com>
2018-02-16 19:03:39 +0600

    changes for volumio

--- sound/soc/codecs/pcm5102a.c
+++ sound/soc/codecs/pcm5102a.c
@@ -25,7 +25,9 @@ static struct snd_soc_dai_driver pcm5102a_dai = {
 	.playback = {
 		.channels_min = 2,
 		.channels_max = 2,
-		.rates = SNDRV_PCM_RATE_8000_384000,
+		.rates = SNDRV_PCM_RATE_8000_192000 | SNDRV_PCM_RATE_KNOT,
+		.rate_min = 8000,
+		.rate_max = 384000,
 		.formats = SNDRV_PCM_FMTBIT_S16_LE |
 			   SNDRV_PCM_FMTBIT_S24_LE |
 			   SNDRV_PCM_FMTBIT_S32_LE
